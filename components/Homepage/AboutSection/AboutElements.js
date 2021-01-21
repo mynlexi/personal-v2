0c9206b2bbacc25791c1f799251a14e074253474
lorenz westner <mynlexi@gmail.com>
2021-01-21 19:54:19 +0100

    effect project cards

--- components/Homepage/AboutSection/AboutElements.js
+++ components/Homepage/AboutSection/AboutElements.js
@@ -57,12 +57,13 @@ export const AboutSkillList = styled.ul`
 
 
 export const AboutImgBlock = styled.div`
-position: relative;
-max-width: 300px;
-@media (max-width: 768px) {
-  margin: 50px auto 0;
-  width: 70%;
-}
+  position: relative;
+  max-width: 300px;
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
 .wrapper {
   display: block;
   position: relative;
@@ -82,35 +83,36 @@ max-width: 300px;
       mix-blend-mode: normal;
     }
   }
+
   .img {
     position: relative;
     border-radius: var(--border-radius);
     mix-blend-mode: multiply;
     filter: grayscale(100%) contrast(1);
     transition: var(--transition);
-  }
-  &:before,
-  &:after {
-    content: '';
-    display: block;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    border-radius: var(--border-radius);
-    transition: var(--transition);
-  }
-  &:before {
-    top: 0;
-    left: 0;
-    background-color: var(--navy);
-    mix-blend-mode: screen;
-  }
-  &:after {
-    border: 2px solid var(--green);
-    top: 20px;
-    left: 20px;
-    z-index: -1;
-  }
+    }
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
 
 
 `
