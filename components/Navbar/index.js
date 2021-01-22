86899a891673b389328cf81b95a59302c4170de7
lorenz westner <mynlexi@gmail.com>
2021-01-22 13:50:19 +0100

    navbar large screen finito

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,85 +1,168 @@
 import React, {useState, useEffect} from 'react'
 import { FaBars } from 'react-icons/fa'
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink} from './NavbarElements'
-import {animateScroll as scroll } from 'react-scroll'
+import { StyledHeader, StyledNav, StyledLinks} from './NavbarElements'
+import { Link as LinkScroll } from 'react-scroll'
+import { navLinks } from '../../config'
+import Link from 'next/link'
+import { useScrollDirection } from '../Hooks/index';
 
-function Navbar({ toggle }) {
 
-  const [scrollNav, setScrollNav] = useState(false)
 
-  const changeNav = () => {
-    if (window.scrollY >= 80) {
-      setScrollNav(true)
-    } else {
-      setScrollNav(false)
-    }
-  }
+const Nav = ({ isHome }) => {
+  const [isMounted, setIsMounted] = useState(!isHome);
+  const scrollDirection = useScrollDirection('down');
+  const [scrolledToTop, setScrolledToTop] = useState(true);
+
+  const handleScroll = () => {
+    setScrolledToTop(window.pageYOffset < 50);
+  };
 
   useEffect(() => {
-    window.addEventListener('scroll', changeNav)
-  }, [])
+    const timeout = setTimeout(() => {
+      setIsMounted(true);
+    }, 100);
 
-  const toggleHome = () => {
-    scroll.scrollToTop();
-  }
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      clearTimeout(timeout);
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
 
   return (
-    <>
-    <Nav scrollNav={scrollNav}>
-      <NavbarContainer>
-        <NavLogo to="/" onClick={toggleHome}>
-          mynlexi
-        </NavLogo>
-        <MobileIcon onClick={toggle}>
-          <FaBars />
-        </MobileIcon>
-        <NavMenu>
-          <NavItem>
-            <NavLinks 
-            to="about" 
-            smooth={true}
-            duration={500}
-            spy={true}
-            exact='true'
-            offset={-80}
-            >About</NavLinks>
-          </NavItem>
-          <NavItem>
-            <NavLinks 
-            to="projects"
-             smooth={true}
-             duration={500}
-             spy={true}
-             exact='true'
-             offset={-80}>Projects</NavLinks>
-          </NavItem>
-          <NavItem>
-            <NavLinks 
-            to="contact"
-             smooth={true}
-             duration={500}
-             spy={true}
-             exact='true'
-             offset={-80}
-             >Contact</NavLinks>
-          </NavItem>
-     
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink 
-          to="/signin"
-           smooth={true}
-           duration={500}
-           spy={true}
-           exact='true'
-           offset={-80}
-          >Resume</NavBtnLink>
-        </NavBtn>
-      </NavbarContainer>
-    </Nav>
-    </>
+    <StyledHeader scrollDirection={scrollDirection} scrolledToTop={scrolledToTop}>
+      <StyledNav>
+        <div className="logo" tabIndex="-1">
+          {isHome ? ( <LinkScroll to="home"
+                      smooth={true}
+                      duration={500}
+                      spy={true}
+                      exact='true'>
+                        <a href="/" aria-label="home">
+                          &gt; mynlexi
+                        </a></LinkScroll>
+                    ) : (
+                    <Link href="/" aria-label="home">
+                      <a>
+                        &gt; mynlexi
+                      </a>
+                    </Link>
+          )}
+        </div>
 
+        <StyledLinks>
+          <ol>
+          {navLinks &&
+            navLinks.map(({ url, name }, i) => (
+            <li key={i}>
+              <LinkScroll to={url}
+              smooth={true}
+              duration={500}
+              spy={true}
+              exact='true'>
+                {name}
+              </LinkScroll>
+            </li>
+            ))}
+          </ol>
+          <div>
+            <a
+              className="resume-button"
+              href="/resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer">
+              Resume
+            </a>
+          </div>
+        </StyledLinks>              
+      </StyledNav>
+    </StyledHeader>
   );
-}
+};
 
-export default Navbar
+export default Nav;
+
+
+// function Navbar({ toggle }) {
+
+//   const [scrollNav, setScrollNav] = useState(false)
+
+//   const changeNav = () => {
+//     if (window.scrollY >= 80) {
+//       setScrollNav(true)
+//     } else {
+//       setScrollNav(false)
+//     }
+//   }
+
+//   useEffect(() => {
+//     window.addEventListener('scroll', changeNav)
+//   }, [])
+
+//   const toggleHome = () => {
+//     scroll.scrollToTop();
+//   }
+
+//   return (
+//     <>
+//     <Nav scrollNav={scrollNav}>
+//       <NavbarContainer>
+//         <NavLogo to="/" onClick={toggleHome}>
+//           mynlexi
+//         </NavLogo>
+//         <MobileIcon onClick={toggle}>
+//           <FaBars />
+//         </MobileIcon>
+//         <NavMenu>
+//           <NavItem>
+//             <NavLinks 
+//             to="about" 
+//             smooth={true}
+//             duration={500}
+//             spy={true}
+//             exact='true'
+//             offset={-80}
+//             >About</NavLinks>
+//           </NavItem>
+//           <NavItem>
+//             <NavLinks 
+//             to="projects"
+//              smooth={true}
+//              duration={500}
+//              spy={true}
+//              exact='true'
+//              offset={-80}>Projects</NavLinks>
+//           </NavItem>
+//           <NavItem>
+//             <NavLinks 
+//             to="contact"
+//              smooth={true}
+//              duration={500}
+//              spy={true}
+//              exact='true'
+//              offset={-80}
+//              >Contact</NavLinks>
+//           </NavItem>
+     
+//         </NavMenu>
+//         <NavBtn>
+//           <NavBtnLink 
+//           to="/signin"
+//            smooth={true}
+//            duration={500}
+//            spy={true}
+//            exact='true'
+//            offset={-80}
+//           >Resume</NavBtnLink>
+//         </NavBtn>
+//       </NavbarContainer>
+//     </Nav>
+//     </>
+
+//   );
+// }
+
+// export default Navbar
